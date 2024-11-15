5d30d7ecdaf59acf8c90831ae24daa872955a9a7
Fatummm <lumik2006@gmail.com>
2024-11-15 09:33:14 +0300

    cmake test

---
+++ c_language/cmake_test/build/CMakeFiles/MathFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunctions.dir/src/math_functions.cpp.o"
+  "CMakeFiles/MathFunctions.dir/src/math_functions.cpp.o.d"
+  "libMathFunctions.a"
+  "libMathFunctions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c_language/cmake_test/build/CMakeFiles/MathFunctions.dir/src/math_functions.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/MathFunctions.dir/src/math_functions.cpp.o: \
+ /mnt/c/Users/ADM/Documents/GitHub/university/c_language/cmake_test/src/math_functions.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/ADM/Documents/GitHub/university/c_language/cmake_test/include/math_functions.h
