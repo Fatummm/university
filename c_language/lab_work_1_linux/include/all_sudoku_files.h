9981bb1642d8f9cbfd4cf2741a3a456106d20c47
Fatummm <lumik2006@gmail.com>
2024-11-17 13:55:53 +0300

    Goddamn

--- c_language/lab_work_1_linux/include/all_sudoku_files.h
+++
@@ -1,4 +0,0 @@
-#include "../src/sudoku.c"
-#include "../src/console_config_linux.c"
-#include <time.h>
-#include <unistd.h>
--- c_language/lab_work_1_linux/include/user.h
+++
@@ -1,9 +0,0 @@
-#ifndef USER_H
-#define USER_H
-
-void working(short size);
-void clear_brackets(struct Cursor cr, short size);
-void print_brackets(struct Cursor cr, short size);
-short process_input(short *** sud, short size, struct Cursor * curs, short ** dots, char c);
-void print_sudoku_with_cursor(short ** sud, short size, struct Cursor cr);
-#endif
