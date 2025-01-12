da11be0c3debb852399c92522fdf765eae8daed5
Fatum <lumik2006@gmail.com>
2025-01-12 15:27:34 +0300

    not-important changes

--- cpp_language/handbook/2-4/6.cpp
+++ cpp_language/handbook/2-4/6.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 int main() {
-    int m, n, k; std::cin >> m >> n >> k;
-    std::vector<std::vector<char>> v(m, std::vector<char>(n, '0'));
-    for (int i = 0; i != k; ++i) {
-        int x, y; std::cin >> x >> y;
-        v[--x][--y] = '*';
-        for (int ii = std::max(0, x - 1); ii != std::min(x + 2, m); ++ii) {
-            for (size_t jj = std::max(0, y - 1); jj != std::min(y + 2, n); ++jj) {
-                //std::cout << ii << ' ' << jj << '\n';
-                if (v[ii][jj] == '*') continue;
-                v[ii][jj]++; 
-            }
+    std::string s;
+    std::getline(std::cin, s);
+    for (std::string::iterator it = s.begin(); it != s.end(); ++it) {
+        if (*it == ' ') {
+            s.erase(it--);
         }
     }
-    for (int i = 0; i != m; ++i) {
-        for (int j = 0; j != n; ++j) {
-            if (j != 0) std::cout << ' ';
-            std::cout << v[i][j];
-        }
-        std::cout << '\n';
-    }
+    std::string reversed(s.rbegin(), s.rend());
+    if (s == reversed) std::cout << "YES\n";
+    else std::cout << "NO\n";
 }
---
+++ cpp_language/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    for(int i = 0; i != 1'000'000'000; ++i);
+    std::cout << ":)\n";
+}
